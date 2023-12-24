c5ca66544f68fcdff0c5465816c2574782cb41ef
Gal Or <gal.or@post.runi.ac.il>
2023-12-24 18:28:33 +0200

    complete tasks 2-3

--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,17 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-	
+		int side1 = Integer.parseInt(args[0]);
+		int side2 = Integer.parseInt(args[1]);
+		int side3 = Integer.parseInt(args[2]);
+
+		if (side1+side2>side3 & side1+side3>side2 & side2+side3>side1 )
+		{
+			System.out.println(side1 +", "+ side2 +", "+  side3 + ": true");
+		}
+		else
+		{
+			System.out.println(side1 +", "+ side2 +", "+  side3 + ": false");
+		}
 	}
 }
